a0d239d7e456d8037c894132f507bc1fbb2dbe0d
cindyawho <161978639+cindyawho@users.noreply.github.com>
2025-04-16 18:44:02 -0700

    Add sample pokemon ditto info, create pokedex structure, basic style and eventlistener

--- project1-pokedex/script.js
+++ project1-pokedex/script.js
@@ -1,1 +1,7 @@
+const pokedex = document.getElementById("pokedex")
+
+pokedex.addEventListener("click", e => {
+    alert("Welcome to your very own digital Pokedex!");
+})
+
 // using https://pokeapi.co/
