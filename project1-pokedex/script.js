1449fbc20c999dd25c008a733a4c49129f5eb48f
Cindy Andrade <cindya3939@gmail.com>
2025-04-24 09:01:14 -0700

    Create basic getPokemon function, add id's to all relevant Pokemon attribute lines in html

--- project1-pokedex/script.js
+++ project1-pokedex/script.js
@@ -1,7 +1,15 @@
-// const pokedex = document.getElementById("pokedex")
-
-// pokedex.addEventListener("click", e => {
-//     alert("Welcome to your very own digital Pokedex!");
-// })
-
 // using https://pokeapi.co/
+// testing my getPokemon code
+
+async function getPokemon(name) {
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching Pokémon:", error);
+    }
+  }
+
+  // Example usage
+  getPokemon("ditto");
